f389b4485c08f126eb2a719fddac18f8a073f8da
donrestarone <shashikejayatunge@gmail.com>
2018-05-01 17:07:39 -0400

    implemented custom validation for project date being in the future

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,6 +4,14 @@ class Project < ActiveRecord::Base
   has_many :users, through: :pledges # backers
   belongs_to :user # project owner
 
+  validate :project_date_must_be_in_future
   validates :title, :description, :goal, :start_date, :end_date, presence: true
   validates :user, presence: true
+
+  def project_date_must_be_in_future
+  	present_time = Time.now 
+  	if Time.now > self.start_date
+  		errors.add(:start_date, 'project date must be in the future')
+  	end 
+  end 
 end
