d6033cb477bcffe379b8697c333049009a7d36ca
Hasan Naseem <hn92@live.com>
2018-05-02 16:36:32 -0400

    Used factory in pledges

---
+++ test/factories/pledges.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :pledge do
+    dollar_amount 10
+    # user 
+  end
+end
--- test/models/pledge_test.rb
+++ test/models/pledge_test.rb
@@ -3,14 +3,17 @@ require_relative '../test_helper'
 class PledgeTest < ActiveSupport::TestCase
 
   test 'A pledge can be created' do
-    pledge = Pledge.create(
-      dollar_amount: 99.00,
-      project: new_project,
-      user: new_user
-    )
-    pledge.save
-    assert pledge.valid?
-    assert pledge.persisted?
+
+    pledge1 = build(:pledge, user: new_user, project: new_project)
+
+    # pledge = Pledge.create(
+    #   dollar_amount: 99.00,
+    #   project: new_project,
+    #   user: new_user
+    # )
+    pledge1.save
+    assert pledge1.valid?
+    assert pledge1.persisted?
   end
 
   test 'owner cannot back own project' do
@@ -19,7 +22,8 @@ class PledgeTest < ActiveSupport::TestCase
     project = new_project
     project.user = owner
     project.save
-    pledge = Pledge.new( dollar_amount: 3.00, project: project)
+    pledge = build(:pledge, project: project)
+    # pledge = Pledge.new( dollar_amount: 3.00, project: project)
     pledge.user = owner
     pledge.save
     assert pledge.invalid?, 'Owner should not be able to pledge towards own project'
