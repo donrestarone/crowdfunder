7e3f55eb845ed4a1ef6241fd4a0757fc5ee814d4
Hasan Naseem <hn92@live.com>
2018-05-07 17:59:28 -0400

    changed migration

--- db/migrate/20180504191043_drop_useless_tables.rb
+++
@@ -1,7 +0,0 @@
-class DropUselessTables < ActiveRecord::Migration[5.1]
-  def change
-    drop_table :category_tables
-    drop_table :category
-    add_column :categories, :name, :string
-  end
-end
